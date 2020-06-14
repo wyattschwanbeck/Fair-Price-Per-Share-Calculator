8822984e6d63b4546cca3f0a9b892fa37e1210cd
wyattschwanbeck <wyattschwanbeck@gmail.com>
2020-06-14 14:24:05 -0400

    Removed tkinter usage

--- main.py
+++ main.py
@@ -8,60 +8,19 @@ from Calculate_Value_Drivers import Calculate_Value_Drivers
 from FPPS_Excel_Injector import FPPS_Excel_Injector
 from MS_Financial_Downloader import FinancialsDownloader
 import os
-        
-ticker = "DIS"
-
-from tkinter import *
-import os  
-
-class Window(Frame):
-    
-    def __init__(self, master = None):
-        Frame.__init__(self, master)
-        
-        self.master = master
-        self.tickerEntry = None
-        self.init_window()
-        
-        
-    def init_window(self):
-        self.master.title("Fair Price Per Share Generator")
-        #adjust dimensions, expand fills
-        self.pack(fill = BOTH, expand=1)
-        
-        
-        self.tickerEntry = Entry(self.master)
-        self.tickerEntry.place(x=80, y=25)
-        self.tickerEntry.delete(0, END)
-        self.tickerEntry.insert(0,"DIS")
-        
-        
-        RunButton = Button\
-        (self, 
-         text="Produce Modeled Fair Price Per Share",
-         command=self.run_FPPS)
-        RunButton.place(x=40, y=75)
-
-        
-        menu = Menu(self.master)
-        self.master.config(menu = menu)
-        
-        file = Menu(menu)
-        file.add_command(label = "Exit", command = self.client_exit)
-        menu.add_cascade(label = "File", menu = file)
-        
-        edit = Menu(menu)
-        edit.add_command(label = "Reset Defaults")
-        menu.add_cascade(label = "Edit", menu = edit)
 
 
-    def run_FPPS(self):
-        ticker = self.tickerEntry.get()
-        check_path_and_download(ticker)
-        perform_calculations_and_excel_injection(ticker)
+def main():
+    userQuit = False
+    while(userQuit!=True):
+        tickerSym = input("Enter Ticker Symbol or Type Q() to Quit:")
+        if tickerSym=="Q()":
+            userQuit=True
+        else:
+            check_path_and_download(tickerSym)
+            perform_calculations_and_excel_injection(tickerSym)
+
         
-    def client_exit(self):
-        self.master.destroy()
         
 def check_path_and_download(ticker):
     newpath = "{}/".format(ticker)     
@@ -99,12 +58,8 @@ def perform_calculations_and_excel_injection(ticker):
     
     
     c.workbook.close()
+    
+
         
-root = Tk()
-root.geometry("300x150")
 
-
-app = Window(root)
-
-root.mainloop()
-
+main()
